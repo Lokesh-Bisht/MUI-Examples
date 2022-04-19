1a8e62482495c37be26cdccfb5e6534e9c233fa1
Lokesh-Bisht <lokeshbisht.096@gmail.com>
2022-04-19 20:50:46 +0530

    add more input components

--- src/components/inputs/inputs.js
+++ src/components/inputs/inputs.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { ButtonGroups } from './buttonGroups';
 import { Button } from './buttons/button'
+import { Checkboxes } from './checkboxes';
+import { FloatingActionButtons } from './floatingActionButtons';
+import { Radiobuttons } from './radioButtons';
 
 export const Inputs = () => {
   return (
     <div>
       <Button/>
+      <ButtonGroups/>
+      <Checkboxes/>
+      <FloatingActionButtons/>
+      <Radiobuttons/>
     </div>
   );
 }
